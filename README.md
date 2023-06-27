708a12d5771b54e2acb2e2a8ce3d73dabf21b168
Ihyatt <inas.raheema@gmail.com>
2023-06-27 15:29:20 -0700

    compartmentalized start screen + quiz + quiz screen

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ A new Flutter project.
 
 - created classes must extend either stateless or stateful widget
 - Generic Tyoes
-  - generic types are "flexible types" that "work together" with other types
-    - List<string>
-    - List<double>
+    - generic types are "flexible types" that "work together" with other types
+        - List<string>
+        - List<double>
+        - State<DummyWidget>
 ![](../../Desktop/Screenshot 2023-06-27 at 2.22.53 PM.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fundamentals/start_screen.dart';
+import 'package:fundamentals/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 168, 109, 97),
-                Color.fromARGB(255, 223, 176, 166)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }

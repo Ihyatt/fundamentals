32ca0b567cb4e715d0e21366d46d3614f9d9cfb8
Ihyatt <inas.raheema@gmail.com>
2023-06-27 15:03:43 -0700

    added opacity yo image

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,6 +12,7 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: const Color.fromRGBO(255, 245, 245, 0.6),
           ),
           const SizedBox(height: 80),
           const Text(
